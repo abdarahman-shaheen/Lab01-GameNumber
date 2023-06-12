feb46a30a180746d67a969c5fed22216c8a4e5a6
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-06-12 17:14:40 +0300

    Add project files.

---
+++ Lab01-GameNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab01_GameNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
